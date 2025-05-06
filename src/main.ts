009aeed4c1d3aef19b4f6a239bc0db93443e79a1
theyashdedhia <60602133+theyashdedhia@users.noreply.github.com>
2025-05-06 13:58:20 +1000

    Added notify plugin

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-
+import { Quasar, Notify } from 'quasar'
 import App from './App.vue';
 import router from './router';
 import './style.css';
@@ -17,6 +17,12 @@ const app = createApp(App);
 // Use plugins
 app.use(createPinia());
 app.use(router);
+app.use(Quasar, {
+    plugins: {Notify }, 
+    config: {
+      
+    }
+  });
 
 // Mount the app
 app.mount('#app');
